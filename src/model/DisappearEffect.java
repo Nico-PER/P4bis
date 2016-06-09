d2b79ec3c76ded4c395c90dddaffac2f8219f6e8
p1501719 <p1501719@IUTDOUAB2PC49.univ-lyon1.fr>
2016-06-09 09:33:50 +0200

    Ajout class DisappearEffect

---
+++ src/model/DisappearEffect.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author p1501719
+ */
+public class DisappearEffect extends Effect {
+
+    @Override
+    public void playEffect(int line, int column, Game game) {
+
+        int tile_id = game.getBoard().getTileIJ(line, column).getStatus();
+        
+            tile_id = -1;
+        game.getBoard().getTileIJ(line, column).setStatus(tile_id);
+    }
+
+
+    
+}
